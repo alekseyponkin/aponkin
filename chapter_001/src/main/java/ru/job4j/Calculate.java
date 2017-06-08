4f23f12867c95ca9f808467fb269bec987f0369e
Ponkin Aleksey <Ponkin.a.v@gmail.com>
2017-06-08 22:39:41 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,9 +1,16 @@
 package ru.job4j;
 
+/**
+*Class Класс длф вычисления арифметических опереаций +-* / ^.
+*@author Ponkin Aleksey
+*@since 08.06.2017
+*@version 1
+*/
+
 public class Calculate{
 	
 	public static void main (String[] args){
-		Calculate Calc = new Calculate();
+		Calculate сalc = new Calculate();
 		System.out.println("Hello World!");
 	}
 	
