009601f7c67a33c76454c5a793c3925fad516f1e
Ponkin Aleksey <ponkin.a.v@gmail.com>
2017-12-20 17:21:09 +0300

    6.1.1. Считать количество слов и пробелов в тексте.[#1016]

---
+++ chapter_006/src/main/java/ru/job4j/threads/CounterWords.java
@@ -0,0 +1,60 @@
+package ru.job4j.threads;
+
+/**
+ * Class CounterWords.
+ *
+ * @author Ponkin Aleksey
+ * @version 1.0.0
+ * @since 15.12.2017.
+ */
+public class CounterWords implements Runnable {
+    /**
+     * Text for searching.
+     */
+    private String text;
+
+    /**
+     * Constructor class CounterWords.
+     * @param text text for searching.
+     */
+    public CounterWords(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public void run() {
+            System.out.println("Tread count word.");
+            int countWord = 0;
+            for (int i = 0; i < text.length(); i++) {
+                if (text.charAt(i) != ' ') {
+                    while (i < text.length() && text.charAt(i) != ' ') {
+                        i++;
+                    }
+                    countWord++;
+                }
+            }
+            System.out.println("Word : " + countWord);
+    }
+
+    public static void main(String[] args) {
+        String text = "Test  simple   text where 9 word end 11 space";
+        Thread threadCountWords = new Thread(new CounterWords(text));
+        Thread threadCountSpace = new Thread(() -> {
+            System.out.println("Tread count space.");
+            int countSpace = 0;
+            for (int i = 0; i < text.length(); i++) {
+                if (text.charAt(i) == ' ') {
+                    countSpace++;
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Space : " + countSpace);
+        });
+        threadCountSpace.start();
+        threadCountWords.start();
+    }
+}
---
+++ chapter_006/src/main/java/ru/job4j/threads/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Threads.
+ *
+ * @author Ponkin Aleksey (mailto:ponkin.a.v@gmail.com)
+ * @version 0.1
+ */
+
+package ru.job4j.threads;
