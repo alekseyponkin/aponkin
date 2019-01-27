09803d9ad9f1177ef53794ac2bb7611a88007a1c
Ponkin Aleksey <ponkin.a.v@gmail.com>
2019-01-27 21:59:59 +0300

    [Task-2.2.1.2] Add test.

--- IO/src/test/java/ru/job4j/io/RemoveForbiddenWordsTest.java
+++ IO/src/test/java/ru/job4j/io/RemoveForbiddenWordsTest.java
@@ -17,6 +17,9 @@ import static org.junit.Assert.assertThat;
  */
 public class RemoveForbiddenWordsTest {
 
+    /**
+     * Test when contains one abuse word in text.
+     */
     @Test
     public void whenTextContainsOneAbuseWord() {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -26,6 +29,9 @@ public class RemoveForbiddenWordsTest {
         assertThat(out.toString(), is("Test text with one word"));
     }
 
+    /**
+     * Test when contains two abuse word in text.
+     */
     @Test
     public void whenTextContainsTwoAbuseWord() {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -33,5 +39,17 @@ public class RemoveForbiddenWordsTest {
         String[] abuse = new String[] {"abuse", "bad"};
         new RemoveForbiddenWords().dropAbuse(in, out, abuse);
         assertThat(out.toString(), is("Test text with one and one word"));
+    }
+
+    /**
+     * Test when contains two abuse word in text with punctuation char.
+     */
+    @Test
+    public void whenTextContainsTwoAbuseWordAndPunctuationChar() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+    ByteArrayInputStream in = new ByteArrayInputStream("Test text with one abuse and one bad word!".getBytes());
+        String[] abuse = new String[] {"abuse", "bad"};
+        new RemoveForbiddenWords().dropAbuse(in, out, abuse);
+        assertThat(out.toString(), is("Test text with one and one word!"));
     }
 }
