a2315acab864eb729bbe5d6e32fe50c5b51a6cf0
Ponkin Aleksey <ponkin.a.v@gmail.com>
2019-02-01 12:54:05 +0300

    Add test on SearchFiles.

---
+++ IO/src/test/java/ru/job4j/io/SearchFilesTest.java
@@ -0,0 +1,68 @@
+package ru.job4j.io;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Class SearchFilesTest.
+ *
+ * @author Ponkin Aleksey
+ * @version 1.0.0
+ * @since 28.01.2019.
+ */
+public class SearchFilesTest {
+    private static final String SEPARATOR = File.separator;
+    private static String tempDir;
+    private static String tempDirDeep;
+
+   @BeforeClass
+   public static void init() throws IOException {
+       tempDir = System.getProperty("java.io.tmpdir") + "io";
+       tempDirDeep = tempDir + SEPARATOR + "xml";
+       new File(tempDir).mkdir();
+       new File(tempDirDeep).mkdir();
+       new File(tempDir + SEPARATOR + "test.java").createNewFile();
+       new File(tempDir + SEPARATOR + "test.class").createNewFile();
+       new File(tempDirDeep + SEPARATOR + "pom.xml").createNewFile();
+   }
+
+    /**
+     * Test when search one extension in folder.
+     * @throws IOException
+     */
+    @Test
+    public void whenSearchOneExtInFolder() throws IOException {
+        List<File> foundFile = new SearchFiles().files(tempDirDeep, Arrays.asList("xml"));
+        assertThat(foundFile.get(0).getName(), is("pom.xml"));
+    }
+
+    /**
+     * Test when search three extension in folder and sub folder.
+     * @throws IOException
+     */
+    @Test
+    public void whenSearchThreeExtSubFolders() throws IOException {
+        List<File> foundFile = new SearchFiles().files(tempDir, Arrays.asList("java", "class", "xml"));
+        assertThat(foundFile.get(0).getName(), is("test.class"));
+        assertThat(foundFile.get(1).getName(), is("test.java"));
+        assertThat(foundFile.get(2).getName(), is("pom.xml"));
+    }
+
+    /**
+     * Test when search then found nothing.
+     * @throws IOException
+     */
+    @Test
+    public void whenSearchThenFoundNothing() throws IOException {
+        List<File> foundFile = new SearchFiles().files(tempDir, Arrays.asList("txt"));
+        assertTrue(foundFile.isEmpty());
+    }
+}
