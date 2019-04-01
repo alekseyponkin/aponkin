62779aeda81f29d8f10a5e04fc348ffedd681f7f
Ponkin Aleksey <Ponkin.a.v@gmail.com>
2019-04-01 18:56:19 +0300

    [Task-3.3.1.1] Add HibernateContextListener.

---
+++ Hibernate/src/main/java/ru/job4j/todo/listener/HibernateContextListener.java
@@ -0,0 +1,32 @@
+package ru.job4j.todo.listener;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionListener;
+
+@WebListener()
+public class HibernateContextListener implements ServletContextListener,
+        HttpSessionListener, HttpSessionAttributeListener {
+
+    /**
+     * Session factory of Hibernate.
+     */
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        this.sessionFactory = new Configuration().configure().buildSessionFactory();
+        sce.getServletContext().setAttribute("sessionFactory", sessionFactory);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        this.sessionFactory.close();
+    }
+}
