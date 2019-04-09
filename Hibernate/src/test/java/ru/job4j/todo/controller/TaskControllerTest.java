87d03e8b5c4cd4b720c90f20bcccd86bb5623e05
Ponkin Aleksey <Ponkin.a.v@gmail.com>
2019-04-09 12:47:04 +0300

    [Task-3.3.1.1] Refactoring of TaskControllerTest.

--- Hibernate/src/test/java/ru/job4j/todo/controller/TaskControllerTest.java
+++ Hibernate/src/test/java/ru/job4j/todo/controller/TaskControllerTest.java
@@ -39,58 +39,64 @@ import static ru.job4j.todo.controller.ConnectionRollback.create;
  * @since 05.04.2019.
  */
 @RunWith(PowerMockRunner.class)
-@PowerMockIgnore({ "javax.xml.*", "org.xml.sax.*", "com.sun.xml.*", "com.sun.org.*"})
+@PowerMockIgnore({ "javax.xml.*", "org.xml.sax.*", "com.sun.xml.*", "com.sun.org.*", "javax.net.ssl.*", "javax.security.*"})
 public class TaskControllerTest {
 
     private final static SessionFactory FACTORY = new Configuration().configure().buildSessionFactory();
     private static SessionFactory proxyFactory;
+    HttpServletRequest req = mock(HttpServletRequest.class);
+    HttpServletResponse resp = mock(HttpServletResponse.class);
+    ServletConfig config = mock(ServletConfig.class);
+    ServletContext context = mock(ServletContext.class);
+    ByteArrayOutputStream out;
+    PrintWriter writer;
 
     @Before
-    public void beforeMethod() {
+    public void beforeMethod() throws IOException {
         proxyFactory = create(FACTORY);
-    }
-
-    @Test
-    public void whenCallDoGetAllTaskThenCheckTaskInDB() throws IOException, ServletException {
-        IServiceTask service = new ServiceTaskHibernateImpl(proxyFactory);
-
-        Task task = new Task("Du something", new Timestamp(System.currentTimeMillis()));
-        service.add(task);
-
-        Task task2 = new Task("Test", new Timestamp(System.currentTimeMillis()));
-        service.add(task2);
-
-        HttpServletRequest req = mock(HttpServletRequest.class);
-        HttpServletResponse resp = mock(HttpServletResponse.class);
-        ServletConfig config = mock(ServletConfig.class);
-        ServletContext context = mock(ServletContext.class);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PrintWriter writer = new PrintWriter(out);
+        out = new ByteArrayOutputStream();
+        writer = new PrintWriter(out);
         when(resp.getWriter()).thenReturn(writer);
         when(config.getServletContext()).thenReturn(context);
         when(context.getAttribute("sessionFactory")).thenReturn(proxyFactory);
+    }
+
+    @Test
+    public void whenCallDoGetAllTaskThenCheckJson() throws IOException, ServletException {
+        IServiceTask service = new ServiceTaskHibernateImpl(proxyFactory);
+        service.add(new Task("Not done task", new Timestamp(System.currentTimeMillis()), false));
+        service.add(new Task("Done task", new Timestamp(System.currentTimeMillis()), true));
+
         when(req.getParameter("done")).thenReturn("all");
         TaskController controller = new TaskController();
         controller.init(config);
         controller.doGet(req, resp);
-        assertTrue(out.toString().contains("\"description\":\"Du something\""));
-        assertTrue(out.toString().contains("\"description\":\"Test\""));
+
+        assertThat(service.findAll().size(), is(2));
+        assertTrue(out.toString().contains("\"description\":\"Not done task\""));
+        assertTrue(out.toString().contains("\"description\":\"Done task\""));
     }
 
     @Test
-    public void doPost() throws ServletException, IOException {
-        HttpServletRequest req = mock(HttpServletRequest.class);
-        HttpServletResponse resp = mock(HttpServletResponse.class);
+    public void whenCallDoGetNotDoneTaskThenCheckJson() throws IOException, ServletException {
+        IServiceTask service = new ServiceTaskHibernateImpl(proxyFactory);
+        service.add(new Task("Not done task", new Timestamp(System.currentTimeMillis()), false));
+        service.add(new Task("Done task", new Timestamp(System.currentTimeMillis()), true));
 
+        when(req.getParameter("done")).thenReturn("notDone");
+        TaskController controller = new TaskController();
+        controller.init(config);
+        controller.doGet(req, resp);
+
+        assertTrue(out.toString().contains("\"description\":\"Not done task\""));
+        assertFalse(out.toString().contains("\"description\":\"Done task\""));
+    }
+
+    @Test
+    public void whenCallDoPostThenCheckInDb() throws ServletException, IOException {
+        IServiceTask service = new ServiceTaskHibernateImpl(proxyFactory);
         InputStream in = new ByteArrayInputStream("{\"description\":\"Run method doPost\",\"dueDate\":\"2019-04-21T03:33\"}".getBytes());
         ServletInputStream servlet = mock(ServletInputStream.class);
-        ServletConfig config = mock(ServletConfig.class);
-        ServletContext context = mock(ServletContext.class);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PrintWriter writer = new PrintWriter(out);
-        when(resp.getWriter()).thenReturn(writer);
-        when(config.getServletContext()).thenReturn(context);
-        when(context.getAttribute("sessionFactory")).thenReturn(proxyFactory);
         when(servlet.read(
                 Matchers.<byte[]>any(), anyInt(), anyInt()))
                 .thenAnswer((Answer<Integer>) invocationOnMock -> {
@@ -101,33 +107,24 @@ public class TaskControllerTest {
                     return in.read(output, offset, length);
                 });
         when(req.getInputStream()).thenReturn(servlet);
+
         TaskController controller = new TaskController();
         controller.init(config);
         controller.doPost(req, resp);
-        IServiceTask service = new ServiceTaskHibernateImpl(proxyFactory);
+
         assertThat(service.findAll().get(0).getDescription(), is("Run method doPost"));
     }
 
     @Test
-    public void doPut() throws IOException, ServletException {
+    public void whenCallDoPutThenCheckTaskInDb() throws IOException, ServletException {
         IServiceTask service = new ServiceTaskHibernateImpl(proxyFactory);
-        Task task = new Task("Du something", new Timestamp(System.currentTimeMillis()));
+        Task task = new Task("Du something", new Timestamp(System.currentTimeMillis()), false);
         Integer id = service.add(task);
         assertFalse(service.findAll().get(0).isDone());
 
-        HttpServletRequest req = mock(HttpServletRequest.class);
-        HttpServletResponse resp = mock(HttpServletResponse.class);
         String jsonTask = "{\"id\":\"" + id + "\",\"done\":true}";
-
         InputStream in = new ByteArrayInputStream(jsonTask.getBytes());
         ServletInputStream servlet = mock(ServletInputStream.class);
-        ServletConfig config = mock(ServletConfig.class);
-        ServletContext context = mock(ServletContext.class);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PrintWriter writer = new PrintWriter(out);
-        when(resp.getWriter()).thenReturn(writer);
-        when(config.getServletContext()).thenReturn(context);
-        when(context.getAttribute("sessionFactory")).thenReturn(proxyFactory);
         when(servlet.read(
                 Matchers.<byte[]>any(), anyInt(), anyInt()))
                 .thenAnswer((Answer<Integer>) invocationOnMock -> {
@@ -138,32 +135,23 @@ public class TaskControllerTest {
                     return in.read(output, offset, length);
                 });
         when(req.getInputStream()).thenReturn(servlet);
+
         TaskController controller = new TaskController();
         controller.init(config);
         controller.doPut(req, resp);
+
         assertTrue(service.findAll().get(0).isDone());
     }
 
     @Test
-    public void doDelete() throws IOException, ServletException {
+    public void whenCallDoDeleteThenCheckTaskInDb() throws IOException, ServletException {
         IServiceTask service = new ServiceTaskHibernateImpl(FACTORY);
-        Task task = new Task("Run method doDelete", new Timestamp(System.currentTimeMillis()));
-        Integer id = service.add(task);
+        Integer id = service.add(new Task("Run method doDelete", new Timestamp(System.currentTimeMillis()), false));
         assertThat(service.findById(id).getDescription(), is("Run method doDelete"));
 
-        HttpServletRequest req = mock(HttpServletRequest.class);
-        HttpServletResponse resp = mock(HttpServletResponse.class);
         String jsonTask = "{\"id\":\"" + id + "\"}";
-
         InputStream in = new ByteArrayInputStream(jsonTask.getBytes());
         ServletInputStream servlet = mock(ServletInputStream.class);
-        ServletConfig config = mock(ServletConfig.class);
-        ServletContext context = mock(ServletContext.class);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PrintWriter writer = new PrintWriter(out);
-        when(resp.getWriter()).thenReturn(writer);
-        when(config.getServletContext()).thenReturn(context);
-        when(context.getAttribute("sessionFactory")).thenReturn(FACTORY);
         when(servlet.read(
                 Matchers.<byte[]>any(), anyInt(), anyInt()))
                 .thenAnswer((Answer<Integer>) invocationOnMock -> {
@@ -174,9 +162,12 @@ public class TaskControllerTest {
                     return in.read(output, offset, length);
                 });
         when(req.getInputStream()).thenReturn(servlet);
+
+        when(context.getAttribute("sessionFactory")).thenReturn(FACTORY);
         TaskController controller = new TaskController();
         controller.init(config);
         controller.doDelete(req, resp);
+
         assertTrue(service.findAll().isEmpty());
     }
 
